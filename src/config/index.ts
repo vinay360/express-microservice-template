b40cf4f5ba91af6b36433609e0d9e0dcbf4cc76f
Vinay Agarwal <vinayagarwal360@gmail.com>
2023-08-15 18:19:23 +0530

    Config file update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,20 +1,18 @@
-import {config} from "dotenv";
-import * as process from "process";
-import {object, string} from "zod";
+import { config } from 'dotenv';
+import * as process from 'process';
+import { object, string } from 'zod';
 
-config({path: `.env.${process.env.NODE_ENV || 'dev'}`});
-
-const envVariableSchema = object({
-    NODE_ENV: string({required_error: 'NODE_ENV is required'}).nonempty(),
-    PORT: string({required_error: 'PORT is required'}).nonempty(),
-    ADDRESS: string().default('0.0.0.0'),
-    MONGODB_URI: string({required_error: 'MONGODB_URI is required'})
+config({
+  path: `.env.${process.env.NODE_ENV?.toString().slice(0, 3) || 'dev'}`,
 });
 
-export const {
-    NODE_ENV,
-    ADDRESS,
-    PORT,
-    MONGODB_URI
-} = envVariableSchema.parse(process.env);
+const envVariableSchema = object({
+  NODE_ENV: string({ required_error: 'NODE_ENV is required' }).nonempty(),
+  PORT: string({ required_error: 'PORT is required' }).nonempty(),
+  ADDRESS: string().default('0.0.0.0'),
+  MONGODB_URI: string({ required_error: 'MONGODB_URI is required' }),
+});
 
+export const { NODE_ENV, ADDRESS, PORT, MONGODB_URI } = envVariableSchema.parse(
+  process.env
+);
